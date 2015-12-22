1ae7ee0340e9a348a6cd454adf8979eba5589b8b
NurullahCinar <nrllhcinar@gmail.com>
2015-12-22 23:33:44 +0200

    Initial Commit

---
+++ src/sezarŞifreleyici/CeaserChipher.java
@@ -0,0 +1,47 @@
+package sezarﬁifreleyici;
+
+public class CeaserChipher {
+	
+	 private static String message = "message";
+	 private static int encyrptMode = 3;
+	
+	
+	 public static void main(String[] args) {
+	  
+	    String encyrptedMessage = encyrptMessage(message, encyrptMode);
+	    System.out.print("Mesaj: " + message +"\n");
+	    System.out.print("ﬁifreli Mesaj: " + encyrptedMessage);  
+	    
+	    
+	    
+	 }
+	 
+	 private static String encyrptMessage(String message, int mode){
+		 String encyrptedMessage = "";
+		 
+		 for (int i = 0; i < message.length(); i++)
+		      encyrptedMessage += letterChipher(message.charAt(i), mode);
+		 
+		return encyrptedMessage;
+	 }
+	 
+	 private static String decyrptMessage(String message, int mode){
+		 String decyrptedMessage = "";
+		 
+		 for (int i = 0; i < message.length(); i++)
+		      decyrptedMessage += letterChipher(message.charAt(i), mode);
+		 
+		return decyrptedMessage;
+	 }
+	 
+	  private static char letterChipher(char ch, int mode) {
+	    
+		  if (!Character.isLetter(ch))
+	      return ch;
+	    
+		  if (Character.isUpperCase(ch))
+			  return (char) (((ch - 'A') + mode) % 26 + 'A');
+	    
+		  return (char) (((ch - 'a') + mode) % 26 + 'a');
+	  }
+}
